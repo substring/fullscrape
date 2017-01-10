07ff19476526dfc5ab8546c3ea49a8f8b3481c68
Subs <frog2wah@gmail.com>
2017-01-10 23:55:16 +0100

    Now handles Screenscraper + added new systems + better handling of hash Closes https://github.com/substring/fullscrape/issues/7 https://github.com/substring/fullscrape/issues/2

--- fullscrape.sh
+++ fullscrape.sh
@@ -1,19 +1,47 @@
 #!/bin/bash
 
+function getLang {
+  rbxLocale=$(grep "^system.language" recalbox.conf | cut -d "=" -f 2 | cut -d "_" -f 1 )
+  echo "${rbxLocale}"
+}
+
+function showHelp {
+  echo "Usage : "
+  echo "-u : update - only scrape new roms"
+}
+
+
+
+
 ROMSPATH=/recalbox/share/roms
 GAMELISTPATH=$ROMSPATH
 IMAGESPATH=$ROMSPATH
 TMP=/tmp
 SCRAPER=$TMP/scraper
 MAMEDEVICES=("mame" "fba" "fba_libretro" "neogeo")
-CANSCRAPE=("${MAMEDEVICES[@]}" "nes" "snes" "n64" "gb" "gbc" "gba" "megadrive" "mastersystem" "sega32x" "gamegear" "pcengine" "atari2600" "lynx" "psx" "scummvm" "segacd" "virtualboy" "sg1000" "ngp" "ngpc" "fds")
+CANSCRAPE=("${MAMEDEVICES[@]}" "atari2600" "atari7800" "lynx" "colecovision" "gw" "vectrex" "o2em" "fds" "nes" "snes" "n64" "gb" "gbc" "gba" "virtualboy" "megadrive" "mastersystem" "sega32x" "segacd" "sg1000" "gamegear" "dreamcast" "pcengine" "supergrafx" "psx" "scummvm" "ngp" "ngpc")
 ESDAEMON=$(ls /etc/init.d/S*emulationstation)
 
 selectiveMode=0
 updateMode=
+screenScraper=
 
-# if $1 = -u, then set to update only mode
-[ "$1" = '-u' ] && updateMode="-append" && shift
+OPTIND=1
+
+while getopts "h?us" opt; do
+    case "$opt" in
+    h|\?)
+        showHelp
+        exit 0
+        ;;
+    u)  updateMode="-append"
+        ;;
+    #~ s)  screenScraper="-use_ss=true -use_gdb=false `getLang`"
+        #~ ;;
+    esac
+done
+
+shift $((OPTIND-1))
 
 # check if some system names are passed as arguments
 if [ $# -gt 0 ]
@@ -21,6 +49,11 @@ then
   selectiveMode=1
   selectedSystems=( "$*" )
 fi
+
+echo "-u : $updateMode"
+echo "-s : $screenScraper"
+echo "Remaining args : $selectedSystems"
+#exit 0
 
 # rpi or rpi2 ?
 uname -a | grep 'armv7' >/dev/null
@@ -58,6 +91,10 @@ then
   while kill -0 "$espid" > /dev/null 2>&1 ; do echo -n "." ;sleep 1; done
 fi
 
+rbxLang=`getLang`
+# Run an empty scraper to get hash files
+$SCRAPER -console_src=ss,gdb,ovgdb -mame_src=ss,gdb
+
 #echo "$ROMSPATH/fba" | while read device
 find $ROMSPATH -type d -maxdepth 1 | grep -v "^${ROMSPATH}$"| while read device
 do
@@ -73,21 +110,31 @@ do
   #Test if it's an arcade system
   if [[ " ${MAMEDEVICES[@]} " =~ " ${system} " ]]
   then
-    arcade="-mame -mame_img=\"m,t,s\""
+    arcade="-mame -mame_img=\"s,t,m,c\""
   else
     arcade=""
   fi
-
+  # Disabling ovgdb for now as a source as it slows down the process because it's checking for its hash file everytime
   if [[ " ${CANSCRAPE[@]} " =~ " ${system} " ]]
   then
     echo -e "\n+++ Scraping $system"
-    $SCRAPER $updateMode $arcade $imgparms -rom_dir="$device" -output_file="$GAMELISTPATH/$system/gamelist.xml" -workers=$nbworkers -img_workers=1 -image_path="./downloaded_images" -image_dir="$IMAGESPATH/$system/downloaded_images"
+    $SCRAPER $updateMode $arcade $imgparms \
+      -hash_file=/recalbox/share/system/.sselph-scraper/hash.csv \
+      -rom_dir="$device" \
+      -output_file="$GAMELISTPATH/$system/gamelist.xml" \
+      -workers=$nbworkers \
+      -console_src=ss,gdb -mame_src=ss,gdb \
+      -console_img=s,b,f,a,l,3b \
+      -lang=$rbxLang \
+      -img_workers=1 \
+      -image_path="./downloaded_images" \
+      -image_dir="$IMAGESPATH/$system/downloaded_images"
   else
     echo "--- $system is not a scrapable system"
   fi
 done
 
-rm $SCRAPER
+#rm $SCRAPER
 
 if [ $restartES -eq "1" ]
 then
